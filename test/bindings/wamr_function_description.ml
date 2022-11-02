2fd2752e1ae8835b90d5feb3e31b350d4850a0f9
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-11-01 21:18:56 -0700

    Some more bindings and running an actual wasm module

--- test/bindings/wamr_function_description.ml
+++ test/bindings/wamr_function_description.ml
@@ -1,6 +1,6 @@
 open Ctypes
 
-(* module Types = Types_generated *)
+module Types = Types_generated
 
 module Functions (F : Ctypes.FOREIGN) = struct
   open F
@@ -8,5 +8,15 @@ module Functions (F : Ctypes.FOREIGN) = struct
   let wasm_runtime_init = foreign "wasm_runtime_init" (void @-> returning bool)
 
   let wasm_runtime_destroy = foreign "wasm_runtime_destroy" (void @-> returning void)
+
+  let wasm_runtime_load = foreign "wasm_runtime_load" (ptr uint8_t @-> uint32_t @-> ptr char @-> uint32_t @-> returning Types.wasm_module_t)
+
+  let wasm_runtime_instantiate = foreign "wasm_runtime_instantiate" (Types.wasm_module_t @-> uint32_t @-> uint32_t @-> ptr char @-> uint32_t @-> returning Types.wasm_module_inst_t)
+
+  let wasm_application_execute_main = foreign "wasm_application_execute_main" (Types.wasm_module_inst_t @-> int32_t @-> ptr (ptr char) @-> returning bool)
+
+  let wasm_application_execute_func = foreign "wasm_application_execute_func" (Types.wasm_module_inst_t @-> ptr char @-> int32_t @-> ptr (ptr char) @-> returning bool)
+
+  let wasm_runtime_set_wasi_args_ex = foreign "wasm_runtime_set_wasi_args_ex" (Types.wasm_module_t @-> ptr (ptr char) @-> uint32_t @-> ptr (ptr char) @-> uint32_t @-> ptr (ptr char) @-> uint32_t @-> ptr (ptr char) @-> int @-> int @-> int @-> int @-> returning void)
 end
 
--- test/bindings/wamr_type_description.ml
+++ test/bindings/wamr_type_description.ml
@@ -1,4 +1,13 @@
+open Ctypes
 
 module Types (F : Ctypes.TYPE) = struct
   open F
+
+  type wasm_module_t
+
+  let wasm_module_t : wasm_module_t structure ptr typ = ptr (structure "WASMModuleCommon")
+
+  type wasm_module_inst_t
+
+  let wasm_module_inst_t : wasm_module_inst_t structure ptr typ = ptr (structure "WASMModuleInstanceCommon")
 end
