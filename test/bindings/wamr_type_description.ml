08916615c5d9c227e4ad96e53c89497fbc91cae3
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-12-15 15:35:54 -0700

    Rework some bindings to work with bytes & return options

--- test/bindings/wamr_type_description.ml
+++ test/bindings/wamr_type_description.ml
@@ -6,8 +6,10 @@ module Types (F : Ctypes.TYPE) = struct
   type wasm_module_t
 
   let wasm_module_t : wasm_module_t structure ptr typ = ptr (structure "WASMModuleCommon")
+  let wasm_module_t_opt : wasm_module_t structure ptr option typ = ptr_opt (structure "WASMModuleCommon")
 
   type wasm_module_inst_t
 
   let wasm_module_inst_t : wasm_module_inst_t structure ptr typ = ptr (structure "WASMModuleInstanceCommon")
+  let wasm_module_inst_t_opt : wasm_module_inst_t structure ptr option typ = ptr_opt (structure "WASMModuleInstanceCommon")
 end
--- test/test.ml
+++ test/test.ml
@@ -2,30 +2,31 @@ let res = Wamr.Functions.wasm_runtime_init () in
 if res == true then print_endline "wasm_runtime_init success" else print_endline "wasm_runtime_init fail";
 
 let input = open_in_bin "test.gr.wasm" in
-let data = really_input_string input (in_channel_length input) in
+let len = (in_channel_length input) in
+let byts = Bytes.create len in
+let () = really_input input byts 0 len in
 let () = print_endline "Data read" in
 
-let buf = Cstruct_cap.of_string data in
-let buf_start = Ctypes.(bigarray_start array1 (Cstruct_cap.unsafe_to_bigarray buf)) in
-let buf_size = Bigarray.Array1.size_in_bytes (Cstruct_cap.unsafe_to_bigarray buf) in
-let () = print_endline "buf created" in
-let error_buf = Cstruct_cap.create 1000 in
-let error_start = Ctypes.(bigarray_start array1 (Cstruct_cap.unsafe_to_bigarray error_buf)) in
-let error_size = Bigarray.Array1.size_in_bytes (Cstruct_cap.unsafe_to_bigarray error_buf) in
-let charptr_to_uint8ptr = Ctypes.(coerce (ptr char) (ptr uint8_t)) in
-let wasm_mod = Wamr.Functions.wasm_runtime_load (charptr_to_uint8ptr buf_start) (Unsigned.UInt32.of_int buf_size) error_start (Unsigned.UInt32.of_int error_size) in
-let () = print_endline "wasm_mod created" in
-let () = print_endline (Cstruct_cap.to_string error_buf) in
-let nothing = fun () -> Ctypes.(from_voidp (ptr char) null) in
-let zero = fun () -> (Unsigned.UInt32.of_int 0) in
-let () = Wamr.Functions.wasm_runtime_set_wasi_args wasm_mod (nothing ()) (zero ()) (nothing ()) (zero ()) (nothing ()) (zero ()) (nothing ()) 0 in
-let () = print_endline "wasi stuff set" in
-let wasm_mod_inst = Wamr.Functions.wasm_runtime_instantiate wasm_mod (Unsigned.UInt32.of_int 8192) (Unsigned.UInt32.of_int 8192) error_start (Unsigned.UInt32.of_int error_size) in
-let () = print_endline "wasm_mod instantiated" in
-let () = print_endline (Cstruct_cap.to_string error_buf) in
-let start_func_buf = Cstruct_cap.of_string "_start" in
-let start_func_start = Ctypes.(bigarray_start array1 (Cstruct_cap.unsafe_to_bigarray start_func_buf)) in
-let success = Wamr.Functions.wasm_application_execute_func wasm_mod_inst start_func_start (Signed.Int32.of_int 0) (Ctypes.from_voidp (Ctypes.ptr Ctypes.char) Ctypes.null) in
-let () = if success then print_endline "Ran successfully" else print_endline "Failed to run" in
-let () = Wamr.Functions.wasm_runtime_destroy () in
-print_endline "wasm_runtime_destroy"
+let buf_start = Ctypes.ocaml_bytes_start byts in
+let buf_size = Bytes.length byts in
+let error_buf = Bytes.create 128 in
+let error_start = Ctypes.ocaml_bytes_start error_buf in
+let error_size = Bytes.length error_buf in
+let wasm_mod = Wamr.Functions.wasm_runtime_load buf_start (Unsigned.Size_t.of_int buf_size) error_start (Unsigned.Size_t.of_int error_size) in
+let () = match wasm_mod with
+  | None -> print_endline (Bytes.to_string error_buf)
+  | Some(wasm_mod) -> let () = print_endline "wasm_mod created" in
+    let nothing = fun () -> Ctypes.(from_voidp ocaml_bytes null) in
+    let zero = fun () -> (Unsigned.Size_t.of_int 0) in
+    let () = Wamr.Functions.wasm_runtime_set_wasi_args wasm_mod (nothing ()) (zero ()) (nothing ()) (zero ()) (nothing ()) (zero ()) (nothing ()) 0 in
+    let () = print_endline "wasi stuff set" in
+    let wasm_mod_inst = Wamr.Functions.wasm_runtime_instantiate wasm_mod (Unsigned.UInt32.of_int 8192) (Unsigned.UInt32.of_int 8192) error_start (Unsigned.Size_t.of_int error_size) in
+    let () = match (wasm_mod_inst) with
+      | None -> print_endline (Bytes.to_string error_buf)
+      | Some(wasm_mod_inst) -> let () = print_endline "wasm_mod instantiated" in
+        let start_func_buf = Bytes.of_string "_start" in
+        let start_func_start = Ctypes.ocaml_bytes_start start_func_buf in
+        let success = Wamr.Functions.wasm_application_execute_func wasm_mod_inst start_func_start (Signed.Int32.of_int 0) (Ctypes.from_voidp (Ctypes.ptr Ctypes.char) Ctypes.null) in
+        let () = if success then print_endline "Ran successfully" else print_endline "Failed to run" in
+        let () = Wamr.Functions.wasm_runtime_destroy () in
+        print_endline "wasm_runtime_destroy" in () in ()
